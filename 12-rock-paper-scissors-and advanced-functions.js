1f70528420a6b208a8eb0510175b6071517d50c3
Alex <alexnjoroge102@gmail.com>
2025-01-25 18:17:51 +0300

    we removed the onclick feature and replaced it with addEventListener method which is the best practice

--- 12-rock-paper-scissors-and advanced-functions.js
+++ 12-rock-paper-scissors-and advanced-functions.js
@@ -28,6 +28,21 @@
       isAutoPlaying = false;
     }
       }
+
+      document.querySelector('.js-rock-button')
+      .addEventListener('click', () => {
+        playGame('rock');
+      });
+      
+      document.querySelector('.js-paper-button')
+      .addEventListener('click', () => {
+        playGame('paper');
+      });
+      
+      document.querySelector('.js-scissors-button')
+      .addEventListener('click', () => {
+        playGame('scissors');
+      });
       
   
    function playGame(playerMove){
