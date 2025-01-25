dbaf74ef1a6014d7dc6ef9e6105c2bfca109d518
Alex <alexnjoroge102@gmail.com>
2025-01-25 18:26:39 +0300

    added a keyboard feature where when you press r for rock, p for paper, and s for scissors the game will play the moves accordingly. This is an application of addEventlistener with a different event which is keydown

--- 12-rock-paper-scissors-and advanced-functions.js
+++ 12-rock-paper-scissors-and advanced-functions.js
@@ -43,6 +43,17 @@
       .addEventListener('click', () => {
         playGame('scissors');
       });
+
+      document.body.addEventListener('keydown', (event) => {
+        if (event.key === 'r') {
+          playGame('rock');
+        } else if (event.key === 'p'){ 
+          playGame('paper')
+        }
+        else if (event.key === 's'){
+          playGame('scissors')
+        }
+      })
       
   
    function playGame(playerMove){
